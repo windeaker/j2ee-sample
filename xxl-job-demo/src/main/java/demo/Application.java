88774b625c6d3316872c5142b3297b533512414a
windeaker for company <windeaker@foxmail.com>
2021-02-08 18:39:55 +0800

    xxl-job

---
+++ xxl-job-demo/src/main/java/demo/Application.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiong qiang
+ * @Description TODO
+ * @Date 2021/2/8
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
