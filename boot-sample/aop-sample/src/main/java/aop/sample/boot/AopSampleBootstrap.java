2867cdb4bc723bc618dde1fd9e10a5653864e531
windeaker <windeaker@foxmail.com>
2021-04-12 23:23:34 +0800

    优化项目结构

---
+++ boot-sample/aop-sample/src/main/java/aop/sample/boot/AopSampleBootstrap.java
@@ -0,0 +1,16 @@
+package aop.sample.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author winde
+ * @Description TODO
+ * @Date 2021/4/12
+ */
+@SpringBootApplication
+public class AopSampleBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(AopSampleBootstrap.class,args);
+    }
+}
